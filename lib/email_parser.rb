21e3a6b55982a682839151baec6c81b79dcadfce
James Lenahan <james.k.lenahan@gmail.com>
2020-01-06 22:21:33 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -8,3 +8,10 @@ class EmailParser
   def initialize(csv_emails)
     @csv_emails = csv_emails
   end 
+  def parse 
+    csv_emails.split.collect do |address|
+      address.split(',') 
+    end
+    .flatten.uniq 
+  end 
+end 
